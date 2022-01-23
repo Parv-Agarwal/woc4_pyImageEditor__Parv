6aaa3fa298b5ed33dc97cccaa6fca30b2e38ee03
Parv-Agarwal <97544432+Parv-Agarwal@users.noreply.github.com>
2022-01-23 21:17:06 +0530

    Added invert image (button and algorithm)

--- Image_Editor_main.py
+++ Image_Editor_main.py
@@ -1,13 +1,18 @@
 from tkinter import *
 from tkinter import filedialog
 from PIL import Image, ImageTk
+from math import floor
 
 root = Tk()
 root.geometry("1200x700")
 root.title("IMAGE EDITOR")
 
+#Frames defined
 image_frame = LabelFrame(root, padx = 20, pady = 20)        # frame where image is placed
 image_frame.place(relx = 0.3, rely = 0.5, anchor= CENTER)
+colour_change_frame = LabelFrame(root, text = "COLOUR MANIPULATION", padx = 20, pady = 20)
+colour_change_frame.place(relx = 0.85, rely = 0.4, anchor = NE)
+
 image1 = Image.open("Images/scene1.png")
 ims = image1.resize((500,450))
 default_image = ImageTk.PhotoImage(image1.resize((500,450)))
@@ -37,8 +42,8 @@ def flip_vert():      # function for flip vertical
     global image_show
     global image2
     image_show.pack_forget()
-    # algorithm for flip vertical:
 
+    # algorithm for flip vertical:
     for x in range(1, 500):
         for y in range(1,225):
             global p_shift
@@ -70,14 +75,37 @@ def flip_horiz():      # function for flip horizontal
     image_show = Label(image_frame, image = image2)
     image_show.pack()
 
+def inv_img():      # function for invert image
+    global ims
+    global image_show
+    global image2
+    image_show.pack_forget()
+    
+    # algorithm to invert colours:
+    for x in range(1, 500):
+        for y in range(1, 450):
+            global inv_p
+            pixel_access = ims.load()
+            inv_p = (255 - floor(pixel_access[x,y][0]), 255 - floor(pixel_access[x,y][1]), 255 -  floor(pixel_access[x,y][2]))
+            pixel_access[x,y] = inv_p 
+
+    image2 = ImageTk.PhotoImage(ims)
+    image_show = Label(image_frame, image = image2)
+    image_show.pack()
+
+# BUTTONS DEFINAITION
 new_file_button = Button(root, text = "New Image", command= new_image)
 save_file_button = Button(root, text = "Save", command= save_image)
 flip_vert_button = Button(root, text = "FLIP VERTICAL", command = flip_vert, padx = 50, pady = 20)
 flip_horiz_button = Button(root, text = "FLIP HORIZONTAL", command = flip_horiz, padx = 50, pady = 20)
+inv_img_button = Button(colour_change_frame, text = "INVERT IMAGE", command = inv_img, padx = 50, pady = 20)
 
+
+# BUTTONS EXECUTION
 new_file_button.grid(row=0,column=0)
 save_file_button.grid(row = 0, column =1)
 flip_vert_button.place(relx = 0.75, rely = 0.2, anchor= NE)
 flip_horiz_button.place(relx = 0.95, rely = 0.2, anchor= NE)
+inv_img_button.pack()   
 
 root.mainloop()
