191eb804abf946b15ecdedceabec861ccea8815d
Parv-Agarwal <97544432+Parv-Agarwal@users.noreply.github.com>
2022-01-30 02:02:47 +0530

    Face detector(using opencv), insert text function.

--- Image_Editor_FinalEvaluation.py
+++ Image_Editor_FinalEvaluation.py
@@ -1,13 +1,17 @@
+from fileinput import filename
+from opcode import hasnargs
 from tkinter import *
 from tkinter import filedialog, messagebox
-from PIL import Image, ImageTk, ImageEnhance
+from tkinter import colorchooser
+from PIL import Image, ImageTk, ImageEnhance, ImageDraw, ImageFont
 from math import floor
 import math
+from cv2 import cvtColor
 import numpy as np
-import cv2 
+import cv2 as cv
 
 root = Tk()
-root.geometry("1200x700")
+root.geometry("1300x700")
 root.title("IMAGE EDITOR")
 
 # canvas defined
@@ -20,7 +24,7 @@ crop_data_frame.place(relx = 0.78, rely = 0.08, anchor = NE)
 colour_change_frame = LabelFrame(root, text = "COLOUR MANIPULATION", pady = 20)
 colour_change_frame.place(relx = 0.96, rely = 0.33, anchor = NE)
 
-#INPUTS DEFINED FOR CROP IMAGE
+#INPUTS DEFINED
 x1 = Entry(crop_data_frame, width = 10)
 x1.grid(row = 0, column = 1, padx = 5, pady = 4)
 x2 = Entry(crop_data_frame, width = 10)
@@ -29,6 +33,10 @@ y1 = Entry(crop_data_frame, width = 10)
 y1.grid(row = 1, column = 1, padx = 5, pady = 4)
 y2 = Entry(crop_data_frame, width = 10)
 y2.grid(row = 1, column = 4, padx = 5, pady = 4)
+text_entry = Entry(root, width = 50)
+text_entry.place(relx = 0.825, rely= 0.88, anchor = NE)
+
+text_entry_label = Label(root, text = "ENTER TEXT: ").place(relx = 0.535, rely= 0.88)
 
 #Labels in the frame crop_data_frame defined
 wr_x1 = Label(crop_data_frame, text = "x1: ").grid(row = 0, column = 0, pady = 4)
@@ -38,7 +46,8 @@ wr_x1 = Label(crop_data_frame, text = "y2: ").grid(row = 1, column = 3, pady = 4
 rat1 = Label(crop_data_frame, text = "  :  ").grid(row = 0, column = 2, pady = 4)
 rat2 = Label(crop_data_frame, text = "  :  ").grid(row = 1, column = 2, pady = 4)
 
-image1 = Image.open("Images/scene1.png")
+img_filename = "Images/scene1.png"
+image1 = Image.open(img_filename)
 h = image1.height
 w = image1.width
 m = max(h, w)
@@ -57,13 +66,13 @@ image2 = ImageTk.PhotoImage(ims)
 image_show = image_canvas.create_image(335, 250, anchor = CENTER, image = image2)
 
 def new_image():           # function for new image button 
-    global image1 
+    global image1, img_filename
     global image2
     global image_show
     global ims, h, w
     image_canvas.delete(image_show)
-    filename = filedialog.askopenfilename(initialdir= "/woc4_pyImageEditor_Parv/Images", title = "Select an image", filetypes=(("png images", "*.png"), ("jpg images", "*.jpg")))
-    image1 = Image.open(filename)
+    img_filename = filedialog.askopenfilename(initialdir= "/woc4_pyImageEditor_Parv/Images", title = "Select an image", filetypes=(("png images", "*.png"), ("jpg images", "*.jpg")))
+    image1 = Image.open(img_filename)
     h = image1.height
     w = image1.width
     m = max(h, w)
@@ -165,41 +174,7 @@ def rot():      # function for rotating
     global image_show
     global image2
     
-    # algorithm to rotate by using numpy arrays:
-    image = np.array(ims)
-    angle = int(rot_slider.get())  #gettinng angle value from slider
-
-    image_canvas.delete(image_show)
-
-    angle=math.radians(angle)                               #converting degrees to radians
-    cosine=math.cos(angle)
-    sine=math.sin(angle)
-
-    new_height  = round(abs(image.shape[0]*cosine)+abs(image.shape[1]*sine))+1     # height and width of the new image that is to be formed
-    new_width  = round(abs(image.shape[1]*cosine)+abs(image.shape[0]*sine))+1      
-    output=np.zeros((new_height,new_width,image.shape[2]))    # defining output numpy array 
-
-    original_centre_height   = round(((image.shape[0]+1)/2)-1)   # centre of the image about which we have to rotate the image
-    original_centre_width    = round(((image.shape[1]+1)/2)-1)    
-
-    new_centre_height= round(((new_height+1)/2)-1)        # Find the centre of the new image that will be obtained
-    new_centre_width= round(((new_width+1)/2)-1)          
-
-    for i in range(h):
-        for j in range(w):
-            y=image.shape[0]-1-i-original_centre_height           #co-ordinates of pixel with respect to the centre of original image
-            x=image.shape[1]-1-j-original_centre_width                      
-
-            new_y=round(-x*sine+y*cosine)      #co-ordinate of pixel with respect to the rotated image
-            new_x=round(x*cosine+y*sine)
-
-            new_y=new_centre_height-new_y
-            new_x=new_centre_width-new_x
-            if 0 <= new_x < new_width and 0 <= new_y < new_height and new_x>=0 and new_y>=0:
-                output[new_y,new_x,:]=image[i,j,:]    
-    h = new_height
-    w = new_width
-    ims = Image.fromarray((output).astype(np.uint8)) 
+    ims = ims.rotate(rot_slider.get())
     image2 = ImageTk.PhotoImage(ims)
     image_canvas.create_image(335, 250, anchor = CENTER, image = image2)
     rot_slider.set(0)
@@ -338,19 +313,62 @@ def ret_img():              #function for retrieving image
     else:
         return
 
+def insert_text():
+    global text, font_entry, x, y
+    font_entry = colorchooser.askcolor()[1]
+    def change_position(event):
+        global text, x, y
+        x = event.x
+        y = event.y
+
+        if text in image_canvas.find_overlapping(str(x-10), str(y-10), str(x+10), str(y+10)):
+            image_canvas.coords(text, x, y)  # move text to mouse position
+    
+    text = image_canvas.create_text(50, 50, text= text_entry.get(), fill= font_entry, font=('arial', 20))
+    image_canvas.bind("<B1-Motion>", change_position)
+
+def fix_pos():
+    global ims, x, y, font_entry
+    global image_show
+    global image2
+    image_canvas.delete(image_show)
+    text_font = ImageFont.truetype("arial", 26)
+    fix_text = ImageDraw.Draw(ims)
+    fix_text.text((x-40, y-50), text_entry.get(), font_entry, font = text_font)
+    image2 = ImageTk.PhotoImage(ims)
+    image_canvas.create_image(335, 250, anchor = CENTER, image = image2)
+    text_entry.delete(0, END)
+
+def face_det():
+    global img_filename, h, w 
+    img_read = cv.imread(img_filename)
+    img_read = cv.resize(img_read, (w, h))
+    img_gray = cv.cvtColor(img_read, cv.COLOR_BGR2GRAY)
+
+    #algorithm for face detection
+    haar_cascade_face = cv.CascadeClassifier("haar_FaceDetect.xml")      
+    face_det_rect = haar_cascade_face.detectMultiScale(img_gray, scaleFactor = 1.1, minNeighbors = 4)
+    for (x, y, w_f, h_f) in face_det_rect:                                 #making rectangle around detected face
+        cv.rectangle(img_read, (x, y), (x + w_f, y + h_f), (0,255,0), thickness = 2)           
+    
+    cv.imshow("Detected images", img_read)
+
 # BUTTONS DEFINAITION
 new_file_button = Button(root, text = "New Image", command= new_image)
 save_file_button = Button(root, text = "Save", command= save_image)
 flip_vert_button = Button(root, text = "FLIP VERTICAL", command = flip_vert, padx = 50, pady = 20)
 flip_horiz_button = Button(root, text = "FLIP HORIZONTAL", command = flip_horiz, padx = 50, pady = 20)
 inv_img_button = Button(colour_change_frame, text = "INVERT IMAGE", command = inv_img, padx = 30, pady = 15)
-rot_button = Button(root, text = "ROTATE IMAGE", command = rot, padx = 50, pady = 20)
-image_sat_button = Button(root, text = "APPLY SATURATION", command = image_sat, padx = 20, pady = 10)
-image_sharp_button = Button(root, text = "APPLY SHARPNESS", command = image_sharp, padx = 20, pady = 10)
-image_exp_button = Button(root, text = "APPLY EXPOSURE", command = image_exp, padx = 20, pady = 10)
+rot_button = Button(root, text = "ROTATE IMAGE", command = rot, padx = 30, pady = 8)
+image_sat_button = Button(root, text = "APPLY SATURATION", command = image_sat, padx = 20, pady = 5)
+image_sharp_button = Button(root, text = "APPLY SHARPNESS", command = image_sharp, padx = 20, pady = 5)
+image_exp_button = Button(root, text = "APPLY EXPOSURE", command = image_exp, padx = 20, pady = 5)
 image_bw_button = Button(colour_change_frame, text = "BLACK AND WHITE IMAGE", command = img_bw, padx = 30, pady = 15)
 crop_button = Button(root, text = "CROP IMAGE", command = crop_img, padx = 50, pady = 20)
 ret_button = Button(root, text = "RETRIEVE ORIGINAL IMAGE", command = ret_img, padx = 30, pady = 10)
+insert_text_button = Button(root, text = "INSERT TEXT", command = insert_text, padx = 10, pady = 5)
+fix_pos_button = Button(root, text = "FIX POSITION", command = fix_pos, padx = 10, pady = 5)
+face_det_button = Button(root, text = "DETECT FACE IN THE IMAGE", command = face_det, padx = 20, pady = 8)
 
 # BUTTONS EXECUTION
 new_file_button.grid(row=0,column=0)
@@ -359,23 +377,26 @@ flip_vert_button.place(relx = 0.75, rely = 0.2, anchor= NE)
 flip_horiz_button.place(relx = 0.95, rely = 0.2, anchor= NE)
 inv_img_button.grid(row = 0, column = 0, padx= 20)   
 rot_button.place(relx = 0.95, rely = 0.54, anchor= NE)
-image_sat_button.place(relx = 0.95, rely = 0.67, anchor= NE)
-image_sharp_button.place(relx = 0.95, rely = 0.8, anchor= NE)
-image_exp_button.place(relx = 0.95, rely = 0.93, anchor= NE)
+image_sat_button.place(relx = 0.95, rely = 0.63, anchor= NE)
+image_sharp_button.place(relx = 0.95, rely = 0.71, anchor= NE)
+image_exp_button.place(relx = 0.95, rely = 0.79, anchor= NE)
 image_bw_button.grid(row = 0, column = 1, padx= 20)   
 crop_button.place(relx = 0.95, rely = 0.08, anchor= NE)
 ret_button.place(relx = 0.85, rely = 0.01, anchor= NE)
+insert_text_button.place(relx = 0.91, rely = 0.865, anchor= NE)
+fix_pos_button.place(relx = 0.994, rely = 0.865, anchor= NE)
+face_det_button.place(relx = 0.2, rely = 0.05, anchor= CENTER)
 
 #SLIDERS
 rot_slider = Scale(root, from_= 0, to=360, length = 180, orient=HORIZONTAL)
-rot_slider.place(relx = 0.75, rely = 0.54, anchor= NE)
+rot_slider.place(relx = 0.75, rely = 0.53, anchor= NE)
 image_sat_slider = Scale(root, from_= -100, to = 100, length = 180, orient=HORIZONTAL, command= image_sat_slide)
 image_sat_slider.set(0)
-image_sat_slider.place(relx = 0.75, rely = 0.67, anchor= NE)
+image_sat_slider.place(relx = 0.75, rely = 0.62, anchor= NE)
 image_sharp_slider = Scale(root, from_= 1, to = 100, length = 180, orient=HORIZONTAL, command= image_sharp_slide)
-image_sharp_slider.place(relx = 0.75, rely = 0.8, anchor= NE)
+image_sharp_slider.place(relx = 0.75, rely = 0.7, anchor= NE)
 image_exp_slider = Scale(root, from_= -100, to = 100, length = 180, orient=HORIZONTAL, command= image_exp_slide)
 image_exp_slider.set(0)
-image_exp_slider.place(relx = 0.75, rely = 0.93, anchor= NE)
+image_exp_slider.place(relx = 0.75, rely = 0.78, anchor= NE)
 
 root.mainloop()
